68bd90a1255aa9dca469f4df98a9c75000fbc3d0
Andrew Salmanov <ads739912@gmail.com>
2023-02-12 12:14:46 +0200

    axios

--- js/main.js
+++ js/main.js
@@ -201,13 +201,18 @@ window.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   };
   //smart card generation
-  getResource('http://localhost:3000/menu')
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => {
+  //     data.forEach(({ img, altimg, title, descr, price }) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //     });
+  //   });
+  axios.get('http://localhost:3000/menu')
     .then(data => {
-      data.forEach(({ img, altimg, title, descr, price }) => {
-        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-      });
-    });
-
+          data.data.forEach(({ img, altimg, title, descr, price }) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+          });
+        });
   //Forms
 
   const forms = document.querySelectorAll('form');
